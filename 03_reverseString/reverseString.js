3895b9d67bda8b0504487169b8b4865b6446250b
GooMoonRyoung <andrew.truong25@gmail.com>
2023-04-16 16:58:20 -0400

    created a function that added letters from a string to the first index of an array and returned the joined string of the indexes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const array = [] 
+    for (const letter of str){
+        array.unshift(letter)
+    }
+    return array.join('')
 };
 
 // Do not edit below this line
