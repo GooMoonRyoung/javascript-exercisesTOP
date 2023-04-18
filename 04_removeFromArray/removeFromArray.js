4e902eb970e203dc7a632f211bec938118825838
GooMoonRyoung <andrew.truong25@gmail.com>
2023-04-17 23:39:54 -0400

    completed the removeFromArray test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg1, arg2, arg3, arg4) {
+    let nArray = []
+    array.forEach(myFunction);
 
-};
-
+    function myFunction(value) {
+        if (!(value === arg1 || value === arg2 || value === arg3 || value === arg4)){
+            nArray.push(value)
+        }
+    }
+    return nArray
+}
 // Do not edit below this line
 module.exports = removeFromArray;
