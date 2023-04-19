bc84e16df79b238cb692524be52da483a69eeb97
GooMoonRyoung <andrew.truong25@gmail.com>
2023-04-18 23:54:04 -0400

    created a function that takes in 2 numbers and added all the values from those 2 numbers. Also filtering out the arguments for only numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,28 @@
 const sumAll = function(num1, num2) {
-    if (typeof(num1)!== 'number' ||typeof(num2)!== 'number'){
+    if (num1 < 0 || num2 < 0 ){
         return 'ERROR'
     }
-    if (num1 < 0 || num2 < 0){
+    if ((typeof num1) !== 'number' || (typeof num2)!== 'number' ){
         return 'ERROR'
+    }
+
+    let lowerNum
+    let higherNum
+
+    if (num1 > num2){
+        lowerNum = num2
+        higherNum = num1
+    } else{
+        lowerNum = num1
+        higherNum = num2
     }
     
+    let sum = 0
+    for (let i = lowerNum; i <= higherNum; i++){
+        sum += i
+    }
+    console.log(sum)
+    return sum
 };
 
 // Do not edit below this line
