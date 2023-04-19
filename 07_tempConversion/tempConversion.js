69ac58611cb4635daaede6602ea6e76c7995ef3b
GooMoonRyoung <andrew.truong25@gmail.com>
2023-04-19 00:24:37 -0400

    successfully converted temp from celsius to freedom units and back

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertTemp = (((fahrenheit - 32) * 5) / 9)
+  let roundedTemp = Math.round(convertTemp * 10) / 10 
+  return roundedTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertTemp = ((celsius * 9 / 5) + 32)
+  let roundedTemp = Math.round(convertTemp * 10) / 10 
+  return roundedTemp
 };
 
 // Do not edit below this line
