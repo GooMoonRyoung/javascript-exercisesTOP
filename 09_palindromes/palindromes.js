c9cac413b59a5d1a8eed912fda8d9fea91994b9c
GooMoonRyoung <Andrewtruong25@gmail.com>
2023-04-26 22:53:48 -0400

    created string input filter for non-/a-z values and started the construction on mirroring logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    //filters string input for anything that isn't an a-z character and removes it
+    for (const i in a){
+        if (!(a[i].toLowerCase() != a[i].toUpperCase())){
+            a.slice(i, 1)
+        }
+    }
+    let middle = Math.round(a.length / 2)
+    console.log(middle)
 };
 
 // Do not edit below this line
