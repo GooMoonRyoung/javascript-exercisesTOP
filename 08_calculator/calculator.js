e01c89014eae88548bc01d17672c4b815b298ca6
GooMoonRyoung <Andrewtruong25@gmail.com>
2023-04-26 22:01:48 -0400

    completed the calculator exercise TOP

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b
+};
+
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const subtract = function() {
-	
+const sum = function(a) {
+  const startVal = 0
+	const sumall = a.reduce((initVal, currNum) => initVal + currNum, startVal)
+  return sumall
 };
 
-const sum = function() {
-	
+const multiply = function(a) {
+  const startVal = 1
+  const multiAll = a.reduce((initVal, currNum) => initVal * currNum, startVal)
+  return multiAll
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1
+  if (a == 0){
+    return 1
+  }else{
+    for (i = a; i >= 1; i--){
+      sum *= i
+    }
+    return sum
+  }
 };
 
 // Do not edit below this line
