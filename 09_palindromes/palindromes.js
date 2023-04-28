de5f3e4bd60eb4759e1121631e5f7cc9ced61a83
GooMoonRyoung <Andrewtruong25@gmail.com>
2023-04-27 23:23:50 -0400

    created a plaindrome checker and it is not returning true

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,38 @@
 const palindromes = function (a) {
     //filters string input for anything that isn't an a-z character and removes it
     for (const i in a){
-        if (!(a[i].toLowerCase() != a[i].toUpperCase())){
+        if (!(a[i].toLowerCase() != a[i].toUpperCase()) || a[i]=== ' '){
             a.slice(i, 1)
+            console.log(a)
         }
     }
+    let even = Math.round(a.length / 2) % 2 === 0
     let middle = Math.round(a.length / 2)
-    console.log(middle)
+
+    function palindrome(indicator, isEven){
+        if (isEven === true){
+            for (let i = 0; i < indicator; i++){
+                if (a[i] === a[i + indicator]){
+                    console.log(a[1])
+                    continue
+                } else{
+                    return false
+                };
+            }
+        } else{
+            for (let i = 0; i <= indicator; i++){
+                if (a[i] === a[i + indicator + 1]){
+                    continue
+                } else{
+                    return false
+                };
+            }
+        }
+        return true
+    };
+    if (palindrome(middle, even) === true){
+        return true
+    };
 };
 
 // Do not edit below this line
