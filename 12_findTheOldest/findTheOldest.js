d821ab4a6b9433c1dd08433824f31f4e3a94df13
GooMoonRyoung <Andrewtruong25@gmail.com>
2023-05-27 19:36:03 -0400

    completed the findTheOldest Assignement

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest
+    let oldestAge = 0 
+    for (let x in people){
+        let age = 0
+        if (!people[x].yearOfDeath){
+            age = 2023 - people[x].yearOfBirth
+        }else{
+            age = people[x].yearOfDeath - people[x].yearOfBirth
+        }
+        if (age > oldestAge){
+            oldest = people[x]
+            oldestAge = age
+        }
+    }
+    return oldest
 };
 
 // Do not edit below this line
